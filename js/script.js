ca8ed6904de6a0cd633d1d069c43651a6489e22e
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-09-06 13:35:26 +0200

    exercise completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-
+        emails:[],
     }
-  }
+  },
+
+methods:{
+    generateEmail(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
+                const randomEmail = response.data.response;
+                this.emails.push(randomEmail)
+            }) 
+        }
+    }
+},
+
+mounted(){
+    this.generateEmail()
+}
+
 }).mount('#app')
